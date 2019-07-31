601850960f4203bd07f5700e5637fb779cd13970
ThreeSixes <joshlucy@gmail.com>
2019-07-31 15:43:03 -0700

    Stub out REST API.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 'use strict';
-
+const express = require("express");
 const yaml = require('js-yaml');
 const {readFileSync} = require('fs');
+
+// Create a global config object.
+var cfg = {};
+
+// Set up Express.
+var app = express();
 
 // Start the server process.
 async function init() {
   console.log("Loading configuration...");
   try {
-    var cfg = yaml.safeLoad(readFileSync('config.yml', 'utf8'));
+    // Actually load our configuration.
+    cfg = yaml.safeLoad(readFileSync('config.yml', 'utf8'));
+
+    // Start listening for connections with Express.
+    app.listen(cfg.service.listen_port, cfg.service.listen_address, () => {
+     console.log("Server listening on " + cfg.service.listen_address + ":" + cfg.service.listen_port);
+    });
+
+    app.get("/:state/:city/temperature", (req, res, next) => {
+      console.log("Request:" + req.connection.remoteAddress + " -> " + req.params.city + ", " + req.params.state);
+      // TODO: Put out an actual response.
+      res.json({'timestamp': new Date().toISOString(), 'temperature': 39});
+    });
+
   } catch (e) {
     console.log("Failed to load configuration file:\n" + e);
     process.exit(1);
