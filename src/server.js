ebe5d350d3c5a41688b18802bc6f60b040229f10
ThreeSixes <joshlucy@gmail.com>
2019-08-01 00:20:08 -0700

    Add sequelize, update comments.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const {readFileSync} = require('fs');
 const request = require("request");
+const sequelize = require("sequelize");
 const util = require("util");
 const yaml = require('js-yaml');
 const weather = require('openweather-apis');
 
 // Create a global config object.
 console.log("Loading configuration...");
+
+// Port to config mgmt and secrets mgmt.
 const cfg = yaml.safeLoad(readFileSync('config.yml', 'utf8'));
 
 // Set up OpenWeatherMap API key
@@ -24,6 +27,10 @@ app.listen(cfg.service.listen_port, cfg.service.listen_address, () => {
 // Handle incoming requests for the state/city/temperature GET route.
 app.get("/:state/:city/temperature", (req, res, next) => {
   console.log("Request: " + req.connection.remoteAddress + " -> " + req.params.city + ", " + req.params.state);
+
+  // TODO: Check Postgres for a cached entry here.
+
+  // Create the necessary parameters to query the OSM geocoding service.
   let query = util.format("?format=json&q=%s,%s,us", req.params.city, req.params.state);
   let options = {
     url: util.format("%s/%s", cfg.openStreetMap.base_url, query),
@@ -39,7 +46,10 @@ app.get("/:state/:city/temperature", (req, res, next) => {
       console.log("Error geocoding: " + error);
       res.status(500).send(JSON.stringlify({'error': 'Could not geocode location.'}));
     } else {
+      // Attempt to get the latitude and longitude of the most relevant place.
       try {
+        // TODO: Check Postgres for a cached entry here.
+
         // Go with the first result.
         const place_data = JSON.parse(body);
         const lat = Number(place_data[0]['lat']);
